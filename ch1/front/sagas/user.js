25b5e26f7cfa3d555dd3487a50918dabedba331c
SeoSang <ddrrpg@naver.com>
2020-02-06 23:13:24 +0900

    saga update

--- ch1/front/sagas/user.js
+++ ch1/front/sagas/user.js
@@ -8,6 +8,7 @@ function* login() {
   try {
     yield call(loginAPI)
     yield put({
+      // put 은 dispatch 의 역할
       type: LOG_IN_SUCCESS
     })
   } catch (e) {
@@ -18,10 +19,19 @@ function* login() {
   }
 }
 function* watchLogin() {
-  yield takeLatest(LOG_IN, login)
+  yield takeLatest(LOG_IN, login) // takeLatest 안에 next 가 들어있어서 LOG_IN 끝나면 알아서 login 실행하는 중단점 만들어준다.
+  //  똑같은거
+  //   while (true) {
+  //     yield take(LOG_IN)
+  //     yield put({
+  //       type: LOG_IN_SUCCESS
+  //     })
+  //   }
+
+  // takeLatest 와 takeEvery 의 차이 = 동시에 여러번을 했을 때 , 마지막 것만 실행하겠다.
+  // 막 클릭하거나 해서 생기는 버그 막아줄 수 있다.
 }
 
 export default function* userSaga() {
-  yield all([])
-  fork(watchLogin)
+  yield all([fork(watchLogin)])
 }
