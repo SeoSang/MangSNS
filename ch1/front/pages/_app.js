1112b0ef7906c7251a00674f397e2cc2b1b5fe33
SeoSang <ddrrpg@naver.com>
2020-02-06 16:34:23 +0900

    redux saga 도입

--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -4,8 +4,10 @@ import PropTypes from "prop-types"
 import withRedux from "next-redux-wrapper"
 import AppLayout from "../components/AppLayout"
 import { Provider } from "react-redux"
+import createSagaMiddleware from "redux-saga"
 import { createStore, compose, applyMiddleware } from "redux"
 import reducer from "../reducers"
+import rootSaga from "../sagas"
 
 const MangSNS = ({ Component, store }) => {
   return (
@@ -29,17 +31,22 @@ MangSNS.propTypes = {
   store: PropTypes.object
 }
 
+// dispatch 후 액션이 실행되기 전 (두 작업 사이에)  middleware 의 역할 수행
 export default withRedux((initialState, options) => {
-  const middlewares = [] // store 에 기능을 추가하거나 변조!
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [sagaMiddleware]
   const composeEnhancers =
-    (!options.isServer &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
-    compose
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : (!options.isServer &&
+          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
+        compose
   const store = createStore(
     reducer,
     initialState,
     composeEnhancers(applyMiddleware(...middlewares))
   )
+  sagaMiddleware.run(rootSaga)
   return store
 })(MangSNS)
--- ch1/front/reducers/user.js
+++ ch1/front/reducers/user.js
@@ -12,11 +12,20 @@ export const initialState = {
 }
 
 export const LOG_IN = "LOG_IN" // 액션의 이름
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS" // 액션의 이름
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE" // 액션의 이름
 export const LOG_OUT = "LOG_OUT" // 액션의 이름
 export const SIGN_UP = "SIGN_UP" // 액션의 이름
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS" // 액션의 이름
 
 export const loginAction = {
   type: LOG_IN
+}
+export const loginSuccessAction = {
+  type: LOG_IN_SUCCESS
+}
+export const loginFailureAction = {
+  type: LOG_IN_FAILURE
 }
 export const logoutAction = {
   type: LOG_OUT
@@ -25,6 +34,11 @@ export const signUpAction = data => {
   return {
     type: SIGN_UP,
     data: data
+  }
+}
+export const signUpSuccessAction = data => {
+  return {
+    type: SIGN_UP_SUCCESS
   }
 }
 
