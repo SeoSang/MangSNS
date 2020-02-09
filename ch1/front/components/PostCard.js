aae382762267741d69a233018cbdf7b036372835
SeoSang <ddrrpg@naver.com>
2020-02-10 01:25:56 +0900

    make comment

--- ch1/front/components/PostCard.js
+++ ch1/front/components/PostCard.js
@@ -1,25 +1,78 @@
-import React from "react"
-import { Card, Button, Icon, Avatar } from "antd"
+import React, { useCallback, useState } from "react"
+import { Card, Button, Icon, Avatar, Form, TextArea, List, Input } from "antd"
+import { useSelector, useDispatch } from "react-redux"
 
 const PostCard = ({ c }) => {
+  const [commentFormOpened, setCommentFormOpened] = useState(false)
+  const [commentText, setCommentText] = useState("")
+  const { me } = useSelector(state => state.user)
+  const dispatch = useDispatch()
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened(prev => !prev)
+  }, [])
+
+  const onSubmitComment = useCallback(e => {
+    e.preventDefault()
+    if (!me) {
+      return alert("로그인이 필요합니다!")
+    }
+    dispatch({
+      type: ADD_COMMENT_REQUEST
+    })
+  }, [])
+
+  const onChangeCommentText = useCallback(e => {
+    setCommentText(e.target.value)
+  }, [])
+
   return (
-    <Card
-      key={+c.createdAt}
-      cover={c.img && <img alt="example" src={c.img} />}
-      actions={[
-        <Icon type="retweet" key="retweet" />,
-        <Icon type="heart" key="heart" />,
-        <Icon type="message" key="message" />,
-        <Icon type="ellipsis" key="ellipsis" />
-      ]}
-      extra={<Button>팔로우</Button>}
-    >
-      <Card.Meta
-        avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
-        title={c.User.nickname}
-        description={c.content}
-      />
-    </Card>
+    <>
+      <Card
+        key={+c.createdAt}
+        cover={c.img && <img alt="example" src={c.img} />}
+        actions={[
+          <Icon type="retweet" key="retweet" />,
+          <Icon type="heart" key="heart" />,
+          <Icon type="message" key="message" onClick={onToggleComment} />,
+          <Icon type="ellipsis" key="ellipsis" />
+        ]}
+        extra={<Button>팔로우</Button>}
+      >
+        <Card.Meta
+          avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
+          title={c.User.nickname}
+          description={c.content}
+        />
+      </Card>
+      {commentFormOpened && (
+        <>
+          <Form onSubmit={onSubmitComment}>
+            <Form.Item>
+              <Input.TextArea rows={4} value={commentText} onChange={onChangeCommentText} />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              보내기
+            </Button>
+          </Form>
+          <List
+            header={`${c.Comments ? c.Comments.length : 0} 댓글`}
+            itemLayout="horizontal"
+            dataSource={c.Comment || []}
+            renderItem={item => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                  datetime={item.createdAt}
+                />
+              </li>
+            )}
+          ></List>
+        </>
+      )}
+    </>
   )
 }
 
--- ch1/front/pages/signup.js
+++ ch1/front/pages/signup.js
@@ -1,11 +1,19 @@
-import React, { memo } from "react"
+import React, { memo, useEffect } from "react"
 import { Form } from "antd"
+import Router from 'next/router'
 import SignupForm from "../components/SignupForm"
 
 const WrappedSignupForm = Form.create({ name: "register" })(SignupForm)
 
 const Signup = () => {
+  useEffect(() => {
+    if (me) {
+      alert('이미 로그인이 된 상태입니다!')
+      Router.push('/')
+    }
+  }, [me && me.id])
   return (
+    Router
     <>
       <div>회원가입</div>
       <WrappedSignupForm />
