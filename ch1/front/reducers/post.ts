2c68903bd09d6ac81c185f0c96a179d4eca11521
SeoSang <ddrrpg@naver.com>
2020-03-09 14:36:23 +0900

    immer 적용중

--- ch1/front/reducers/post.ts
+++ ch1/front/reducers/post.ts
@@ -38,6 +38,8 @@ import {
   REMOVE_POST_SUCCESS,
 } from "../pages/mytypes/reducerTypes"
 
+import produce from "immer"
+
 export const initialState: PostState = {
   mainPosts: [],
   imagePaths: [], // 미리보기 이미지 경로
@@ -52,196 +54,177 @@ export const initialState: PostState = {
 }
 
 const reducer = (state = initialState, action: PostActionTypes) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST: {
-      return {
-        ...state,
-        isAddingPost: true,
-        addPostErrorReason: "",
-        postAdded: false,
+  return produce(state, draft => {
+    switch (action.type) {
+      case ADD_POST_REQUEST: {
+        draft.isAddingPost = true
+        draft.addPostErrorReason = ""
+        draft.postAdded = false
       }
-    }
-    case ADD_POST_SUCCESS: {
-      return {
-        ...state,
-        isAddingPost: false,
-        mainPosts: [action.data, ...state.mainPosts],
-        postAdded: true,
-        imagePaths: [],
+      case ADD_POST_SUCCESS: {
+        draft.isAddingPost = false
+        draft.mainPosts.push(action.data)
+        return {
+          ...state,
+          isAddingPost: false,
+          mainPosts: [action.data, ...state.mainPosts],
+          postAdded: true,
+          imagePaths: [],
+        }
       }
-    }
-    case ADD_POST_FAILURE: {
-      return {
-        ...state,
-        isAddingPost: false,
-        addPostErrorReason: action.error,
+      case ADD_POST_FAILURE: {
+        return {
+          ...state,
+          isAddingPost: false,
+          addPostErrorReason: action.error,
+        }
       }
-    }
-    case REMOVE_POST_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter(v => v.id !== action.data),
+      case REMOVE_POST_SUCCESS: {
+        return {
+          ...state,
+          mainPosts: state.mainPosts.filter(v => v.id !== action.data),
+        }
       }
-    }
-    case LIKE_POST_REQUEST: {
-      return {
-        ...state,
+      case LIKE_POST_REQUEST: {
+        break
       }
-    }
-    case LIKE_POST_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
-      const post = state.mainPosts[postIndex]
-      const Likers = [{ id: action.data.userId }, ...post.Likers!]
-      const mainPosts = [...state.mainPosts]
-      mainPosts[postIndex] = { ...post, Likers }
-      return {
-        ...state,
-        mainPosts,
+      case LIKE_POST_SUCCESS: {
+        const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
+        const post = state.mainPosts[postIndex]
+        const Likers = [{ id: action.data.userId }, ...post.Likers!]
+        const mainPosts = [...state.mainPosts]
+        mainPosts[postIndex] = { ...post, Likers }
+        return {
+          ...state,
+          mainPosts,
+        }
       }
-    }
-    case LIKE_POST_FAILURE: {
-      return {
-        ...state,
-        likeErrorReason: action.error,
+      case LIKE_POST_FAILURE: {
+        return {
+          ...state,
+          likeErrorReason: action.error,
+        }
       }
-    }
-    case UNLIKE_POST_REQUEST: {
-      return {
-        ...state,
+      case UNLIKE_POST_REQUEST: {
+        break
       }
-    }
-    case UNLIKE_POST_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
-      const post = state.mainPosts[postIndex]
-      const Likers = post.Likers ? post.Likers.filter(v => v.id !== action.data.userId) : []
-      const mainPosts = [...state.mainPosts]
-      mainPosts[postIndex] = { ...post, Likers }
-      return {
-        ...state,
-        mainPosts,
+      case UNLIKE_POST_SUCCESS: {
+        const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
+        const post = state.mainPosts[postIndex]
+        const Likers = post.Likers ? post.Likers.filter(v => v.id !== action.data.userId) : []
+        const mainPosts = [...state.mainPosts]
+        mainPosts[postIndex] = { ...post, Likers }
+        return {
+          ...state,
+          mainPosts,
+        }
       }
-    }
-    case UNLIKE_POST_FAILURE: {
-      return {
-        ...state,
-        unlikeErrorReason: action.error,
+      case UNLIKE_POST_FAILURE: {
+        return {
+          ...state,
+          unlikeErrorReason: action.error,
+        }
       }
-    }
-    case RETWEET_REQUEST: {
-      return {
-        ...state,
+      case RETWEET_REQUEST: {
+        break
       }
-    }
-    case RETWEET_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: [action.data, ...state.mainPosts],
+      case RETWEET_SUCCESS: {
+        return {
+          ...state,
+          mainPosts: [action.data, ...state.mainPosts],
+        }
       }
-    }
-    case RETWEET_FAILURE: {
-      return {
-        ...state,
+      case RETWEET_FAILURE: {
+        break
       }
-    }
-    case UPLOAD_IMAGES_REQUEST: {
-      return {
-        ...state,
+      case UPLOAD_IMAGES_REQUEST: {
+        break
       }
-    }
-    case UPLOAD_IMAGES_SUCCESS: {
-      return {
-        ...state,
-        imagePaths: [...state.imagePaths, ...action.data],
+      case UPLOAD_IMAGES_SUCCESS: {
+        action.data.forEach(imgPath => {
+          draft.imagePaths.push(imgPath)
+        })
       }
-    }
-    case UPLOAD_IMAGES_FAILURE: {
-      return {
-        ...state,
+      case UPLOAD_IMAGES_FAILURE: {
+        break
       }
-    }
-    // 포스트 불러오기
-    case LOAD_USER_POSTS_REQUEST:
-    case LOAD_HASHTAG_POSTS_REQUEST:
-    case LOAD_MAIN_POSTS_REQUEST: {
-      return {
-        ...state,
-        mainPosts: [],
+      // 포스트 불러오기
+      case LOAD_USER_POSTS_REQUEST:
+      case LOAD_HASHTAG_POSTS_REQUEST:
+      case LOAD_MAIN_POSTS_REQUEST: {
+        return {
+          ...state,
+          mainPosts: [],
+        }
       }
-    }
-    // 포스트 불러오기
-    case LOAD_USER_POSTS_SUCCESS:
-    case LOAD_MAIN_POSTS_SUCCESS:
-    case LOAD_HASHTAG_POSTS_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: action.data,
+      // 포스트 불러오기
+      case LOAD_USER_POSTS_SUCCESS:
+      case LOAD_MAIN_POSTS_SUCCESS:
+      case LOAD_HASHTAG_POSTS_SUCCESS: {
+        return {
+          ...state,
+          mainPosts: action.data,
+        }
       }
-    }
-    case LOAD_USER_POSTS_FAILURE:
-    case LOAD_HASHTAG_POSTS_FAILURE:
-    case LOAD_MAIN_POSTS_FAILURE: {
-      return {
-        ...state,
+      case LOAD_USER_POSTS_FAILURE:
+      case LOAD_HASHTAG_POSTS_FAILURE:
+      case LOAD_MAIN_POSTS_FAILURE: {
+        break
       }
-    }
-    case ADD_COMMENT_REQUEST: {
-      return {
-        ...state,
-        isAddingComment: true,
-        addCommentErrorReason: "",
-        commentAdded: false,
+      case ADD_COMMENT_REQUEST: {
+        return {
+          ...state,
+          isAddingComment: true,
+          addCommentErrorReason: "",
+          commentAdded: false,
+        }
       }
-    }
-    case ADD_COMMENT_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
-      const post = state.mainPosts[postIndex]
-      console.log("------reducers__post.ts / action.data => ", action.data)
-      console.log("------reducers__post.ts / post.Comments => ", post.Comments)
-      const Comments = post.Comments
-        ? [...post.Comments, action.data.comment]
-        : [action.data.comment]
-      const mainPosts = [...state.mainPosts]
-      mainPosts[postIndex] = { ...post, Comments }
-      return {
-        ...state,
-        isAddingComment: false,
-        mainPosts,
-        commentAdded: true,
+      case ADD_COMMENT_SUCCESS: {
+        const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
+        const post = state.mainPosts[postIndex]
+        console.log("------reducers__post.ts / action.data => ", action.data)
+        console.log("------reducers__post.ts / post.Comments => ", post.Comments)
+        const Comments = post.Comments
+          ? [...post.Comments, action.data.comment]
+          : [action.data.comment]
+        const mainPosts = [...state.mainPosts]
+        mainPosts[postIndex] = { ...post, Comments }
+        return {
+          ...state,
+          isAddingComment: false,
+          mainPosts,
+          commentAdded: true,
+        }
       }
-    }
-    case ADD_COMMENT_FAILURE: {
-      return {
-        ...state,
-        isAddingComment: false,
-        addCommentErrorReason: action.error,
-        commentAdded: false,
+      case ADD_COMMENT_FAILURE: {
+        return {
+          ...state,
+          isAddingComment: false,
+          addCommentErrorReason: action.error,
+          commentAdded: false,
+        }
       }
-    }
-    case LOAD_COMMENTS_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
-      const post = state.mainPosts[postIndex]
-      const Comments = action.data.comments
-      const mainPosts = [...state.mainPosts]
-      mainPosts[postIndex] = { ...post, Comments }
-      console.log("----reducers__post.ts / mainPosts[postIndex] => ", mainPosts[postIndex])
-      return {
-        ...state,
-        mainPosts,
+      case LOAD_COMMENTS_SUCCESS: {
+        const postIndex = state.mainPosts.findIndex(v => v.id === action.data.postId)
+        const post = state.mainPosts[postIndex]
+        const Comments = action.data.comments
+        const mainPosts = [...state.mainPosts]
+        mainPosts[postIndex] = { ...post, Comments }
+        console.log("----reducers__post.ts / mainPosts[postIndex] => ", mainPosts[postIndex])
+        return {
+          ...state,
+          mainPosts,
+        }
       }
-    }
-    case REMOVE_IMAGE: {
-      return {
-        ...state,
-        imagePaths: state.imagePaths.filter((v, i) => i !== action.index),
+      case REMOVE_IMAGE: {
+        draft.imagePaths = state.imagePaths.filter((v, i) => i !== action.index)
       }
-    }
 
-    default: {
-      return {
-        ...state,
+      default: {
+        break
       }
     }
-  }
+  })
 }
 
 export default reducer
